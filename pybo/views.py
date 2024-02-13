167239d2fa9fbf031dc27038f4431a40ec19af3f
Kim In Su <insu9058@naver.com>
2024-02-13 22:13:01 +0900

    2월 13일 장고 공부

--- pybo/views.py
+++ pybo/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
 from .models import Question, Answer
+from .forms import QuestionForm
+
 
 
 
@@ -24,4 +26,14 @@ def answer_create(request, question_id):
     return redirect('pybo:detail',question_id=question_id)
 
 def question_create(request):
-    pass #여기부터하면됨
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {'form':form}
+    return render(request, 'pybo/question_form.html',context)
